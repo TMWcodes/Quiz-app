9a90a8c8fecf10b8db6a75fea4a8b2ecc9d512fc
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-05-17 14:57:07 +0100

    answer test on screen

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
 function selectAnswer() {
@@ -33,7 +43,7 @@ function selectAnswer() {
 
 const questions = [
     {
-        question: 'whats is -',
+        question: 'whats is -?',
         answers: [
             { text: 'right', correct: true},
             { text: 'wrong', correct: false}
