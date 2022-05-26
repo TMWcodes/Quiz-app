edbef267b8657310cad9c86766c8c60924358f08
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-05-26 20:55:01 +0100

    refresh

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while(answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
